eb1e55248de440db9cdc3f1b5007120ef8f98cfc
J_ManG <108377644+J-dbd@users.noreply.github.com>
2024-01-27 19:12:11 +0900

    [Feat] kenematic and ground : movement, propeller (#4)
    * drum's movement and propeller

---
+++ jungle-escape-server/components/movement_backAndForth_X.js
@@ -0,0 +1,19 @@
+var MovementBackAndForthX = pc.createScript("movementBackAndForth_x");
+/** 처음 위치에서 앞뒤(x)로 일정 거리만큼 움직이는 물체에 설정하는 스크립트입니다. */
+
+MovementBackAndForthX.prototype.initialize = function () {
+  this.currPos = this.entity.position;
+};
+
+MovementBackAndForthX.prototype.swap = function (old) {
+  this.currPos = old.currPos;
+};
+// update code called every frame
+MovementBackAndForthX.prototype.update = function (dt) {
+  //console.log("this.currPos", this.currPos);
+  this.entity.setPosition(
+    this.currPos.x - Math.sin(Date.now() / 200) * 2,
+    this.currPos.y,
+    this.currPos.z + Math.sin(Date.now() / 200) * 2
+  );
+};
