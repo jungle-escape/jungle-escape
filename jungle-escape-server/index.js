11d768ce0d6b5f356d7cefa5b4783d2c5a656025
electronyoon <electronyoon@gmail.com>
2024-01-27 11:13:59 +0900

    exempt support for https

--- jungle-escape-server/index.js
+++ jungle-escape-server/index.js
@@ -3,23 +3,24 @@ import 'dotenv/config';
 import fs from 'fs';
 import path from 'path';
 import express from 'express';
-import * as https from 'https';
+// import * as https from 'https';
+import * as http from 'http';
 
-import pn from 'playnetwork';
+import pn from './custom_modules/playnetwork/src/server/index.js';
 
 import FileLevelProvider from './file-level-provider.js';
 
 const app = express();
 
 app.get('/pn.js', (_, res) => {
-    res.sendFile(path.resolve('node_modules/playnetwork/dist/pn.js'));
+    res.sendFile(path.resolve('./custom_modules/playnetwork/dist/pn.js'));
 });
 
-const key = fs.readFileSync('./ssl/localhost.key', 'utf8');
-const cert = fs.readFileSync('./ssl/localhost.crt', 'utf8');
-const credentials = { key, cert };
+// const key = fs.readFileSync('./ssl/localhost.key', 'utf8');
+// const cert = fs.readFileSync('./ssl/localhost.crt', 'utf8');
+// const credentials = { key, cert };
 
-const server = https.createServer(credentials, app);
+const server = http.createServer(app);
 server.listen(8080);
 
 await pn.start({
