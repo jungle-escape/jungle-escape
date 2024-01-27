7229e58b4c6fae5226e023384a42533ab6d4416f
J_ManG <kjamong2020@gmail.com>
2024-01-27 17:36:45 +0900

    [Feat] movement, propeller * Kenematic: drum's movement and propeller

--- jungle-escape-server/components/gate.js
+++ jungle-escape-server/components/gate.js
@@ -1,37 +1,37 @@
-var Gate = pc.createScript('gate');
+var Gate = pc.createScript("gate");
 
-Gate.attributes.add('positionYCurve', { type: 'curve' });
+Gate.attributes.add("positionYCurve", { type: "curve" });
 
 Gate.prototype.initialize = function () {
-    this.defaultPositionY = this.entity.getPosition().y;
-    this.vec3 = new pc.Vec3();
-    this.time = 0;
-    this.activations = 0;
+  this.defaultPositionY = this.entity.getPosition().y;
+  this.vec3 = new pc.Vec3();
+  this.time = 0;
+  this.activations = 0;
 
-    this.entity.on('activation', this.onActivation, this);
+  this.entity.on("activation", this.onActivation, this);
 };
 
 Gate.prototype.swap = function (old) {
-    this.defaultPositionY = old.defaultPositionY;
-    this.vec3 = old.vec3;
-    this.time = old.time;
-    this.activations = old.activations;
+  this.defaultPositionY = old.defaultPositionY;
+  this.vec3 = old.vec3;
+  this.time = old.time;
+  this.activations = old.activations;
 
-    old.entity.off('activation', old.onActivation, old);
-    this.entity.on('activation', this.onActivation, this);
+  old.entity.off("activation", old.onActivation, old);
+  this.entity.on("activation", this.onActivation, this);
 };
 
 Gate.prototype.onActivation = function (activated) {
-    this.activations += activated ? 1 : -1;
+  this.activations += activated ? 1 : -1;
 };
 
 Gate.prototype.update = function (dt) {
-    this.time += this.activations > 0 ? dt : -dt;
+  this.time += this.activations > 0 ? dt : -dt;
 
-    this.time = pc.math.clamp(this.time, 0, 1);
+  this.time = pc.math.clamp(this.time, 0, 1);
 
-    this.vec3.copy(this.entity.getPosition());
-    this.vec3.y = this.defaultPositionY + this.positionYCurve.value(this.time);
+  this.vec3.copy(this.entity.getPosition());
+  this.vec3.y = this.defaultPositionY + this.positionYCurve.value(this.time);
 
-    this.entity.rigidbody.teleport(this.vec3);
+  this.entity.rigidbody.teleport(this.vec3);
 };
