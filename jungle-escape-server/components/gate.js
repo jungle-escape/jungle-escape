3f936597390ec9926d3e38fee36383d415183f48
sumin <91295349+WantKakao@users.noreply.github.com>
2024-02-03 16:14:17 +0900

    helloworld (#30)

--- jungle-escape-server/components/gate.js
+++ jungle-escape-server/components/gate.js
@@ -1,40 +1,26 @@
 var Gate = pc.createScript('gate');
 
-import pn from '../custom_modules/playnetwork/src/server/index.js';
+// import pn from '../custom_modules/playnetwork/src/server/index.js';
 
 Gate.attributes.add('positionYCurve', { type: 'curve' });
 
 Gate.prototype.initialize = function () {
-    this.defaultPositionY = this.entity.getPosition().y;
-    this.vec3 = new pc.Vec3();
+    // this.defaultPositionY = this.entity.getPosition().y;
+    // this.vec3 = new pc.Vec3();
     this.time = 0;
     this.activations = 0;
 
     this.entity.on('activation', this.onActivation, this);
-
-    this.countdown = 4;
-    this.countdownTimer = 0;
-    this.isCountingDown = false;
-    this.elapsedTime = 0;
-    this.isOpened = false;
-    this.isStarted = false;
 };
 
 Gate.prototype.swap = function (old) {
-    this.defaultPositionY = old.defaultPositionY;
-    this.vec3 = old.vec3;
+    // this.defaultPositionY = old.defaultPositionY;
+    // this.vec3 = old.vec3;
     this.time = old.time;
     this.activations = old.activations;
 
     old.entity.off('activation', old.onActivation, old);
     this.entity.on('activation', this.onActivation, this);
-
-    this.countdown = old.countdown;
-    this.countdownTimer = old.countdownTimer;
-    this.isCountingDown = old.isCountingDown;
-    this.elapsedTime = old.elapsedTime;
-    this.isOpened = old.isOpened;
-    this.isStarted = old.isStarted;
 };
 
 Gate.prototype.onActivation = function (activated) {
@@ -51,9 +37,7 @@ Gate.prototype.update = function (dt) {
         // 두 버튼이 모두 활성화되었을 때 게이트 움직임 처리
         this.time += dt;
         if (this.time >= 1 && !this.isCountingDown) {
-            this.isOpened = true;
-            this.isCountingDown = true;
-            this.countdownTimer = 0;
+            this.entity.networkEntity.send('go');
         }
     } else {
         // 두 버튼 중 하나라도 비활성화되면 게이트 움직임 처리
@@ -62,43 +46,43 @@ Gate.prototype.update = function (dt) {
 
     this.time = pc.math.clamp(this.time, 0, 1);
 
-    this.vec3.copy(this.entity.getPosition());
-    if (this.isOpened && this.isCountingDown) {
-        if (this.countdownTimer >= 1 && this.countdown > 0) {
-            this.countdownTimer = 0;
-            this.countdown -= 1;
-            this.entity.networkEntity.send('start', `${this.countdown.toString()}...`);
+    // this.vec3.copy(this.entity.getPosition());
+    // if (this.isOpened && this.isCountingDown) {
+    //     if (this.countdownTimer >= 1 && this.countdown > 0) {
+    //         this.countdownTimer = 0;
+    //         this.countdown -= 1;
+    //         this.entity.networkEntity.send('start', `${this.countdown.toString()}...`);
 
-            if (this.countdown === 0) {
-                this.isCountingDown = false;
-                this.entity.networkEntity.send('start', 'START!!');
-                this.vec3.y = this.defaultPositionY + this.positionYCurve.value(1);
-                this.entity.rigidbody.teleport(this.vec3);
-                this.isStarted = true;
-            }
-        } else {
-            this.countdownTimer += dt;
-        }
-    }
+    //         if (this.countdown === 0) {
+    //             this.isCountingDown = false;
+    //             this.entity.networkEntity.send('start', 'START!!');
+    //             this.vec3.y = this.defaultPositionY + this.positionYCurve.value(1);
+    //             this.entity.rigidbody.teleport(this.vec3);
+    //             this.isStarted = true;
+    //         }
+    //     } else {
+    //         this.countdownTimer += dt;
+    //     }
+    // }
 
-    if (this.isStarted) {
-        this.elapsedTime += dt;
-        this.entity.networkEntity.send('time', this.elapsedTime);
+    // if (this.isStarted) {
+    //     this.elapsedTime += dt;
+    //     this.entity.networkEntity.send('time', this.elapsedTime);
 
-        var list = [];
-        var pointA = new pc.Vec3(0, 0, -620);
-        for (let [id, networkEntity] of pn.networkEntities) {
-            var u = networkEntity;
-            if (u.user) {
-                var pointB = u.rules.position()
-                var distance = pointA.distance(pointB);
-                let info = [distance, `user ${u.user.id}`];
-                list.push(info);
-            }
-        }
-        list.sort((a, b) => {
-            return a[0] - b[0];
-        });
-        this.app.room.send('rank', list);
-    }
+    //     var list = [];
+    //     var pointA = new pc.Vec3(0, 0, -620);
+    //     for (let [id, networkEntity] of pn.networkEntities) {
+    //         var u = networkEntity;
+    //         if (u.user) {
+    //             var pointB = u.rules.position()
+    //             var distance = pointA.distance(pointB);
+    //             let info = [distance, `user ${u.user.id}`];
+    //             list.push(info);
+    //         }
+    //     }
+    //     list.sort((a, b) => {
+    //         return a[0] - b[0];
+    //     });
+    //     this.app.room.send('rank', list);
+    // };
 };
