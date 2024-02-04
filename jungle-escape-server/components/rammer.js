ee150a9445525249aae7fc170de95039bfade676
J_ManG <108377644+J-dbd@users.noreply.github.com>
2024-02-04 18:12:45 +0900

    [Feat] phase 3-2/ floor-attack, teleport (#33)
    * [Feat] kenematic and ground : movement, propeller
        * drum's movement and propeller
    
    * [Feature] cell - attack server-side logic (dup issue)
    
    * [Fix] teleport into server-side

--- jungle-escape-server/components/rammer.js
+++ jungle-escape-server/components/rammer.js
@@ -1,33 +1,35 @@
-var Rammer = pc.createScript('rammer');
+var Rammer = pc.createScript("rammer");
 
-Rammer.attributes.add('positionXCurve', { type: 'curve' });
+Rammer.attributes.add("positionXCurve", { type: "curve" });
 
 Rammer.prototype.initialize = function () {
-    this.defaultPositionX = this.entity.getPosition().x;
-    this.vec3 = new pc.Vec3();
-    this.time = 0;
-    this.direction = 1;
+  this.defaultPositionX = this.entity.getPosition().x;
+  this.vec3 = new pc.Vec3();
+  this.time = 0;
+  this.direction = 1;
 };
 
 Rammer.prototype.swap = function (old) {
-    this.defaultPositionX = old.defaultPositionX;
-    this.vec3 = old.vec3;
-    this.time = old.time;
+  this.defaultPositionX = old.defaultPositionX;
+  this.vec3 = old.vec3;
+  this.time = old.time;
 };
 
 Rammer.prototype.update = function (dt) {
-    const directionX = this.entity.name === 'Rammer01' ? 1 : -1;
-    const speed = this.entity.name === 'Rammer01' ? 1 : 1.5;
+  const directionX = this.entity.name === "Rammer01" ? 1 : -1;
+  const speed = this.entity.name === "Rammer01" ? 1 : 1.5;
 
-    this.time += this.direction * dt * speed;
-    
-    if (this.time > 1 || this.time < 0) {
-        this.direction *= -1;
-        this.time = pc.math.clamp(this.time, 0, 1);
-    }
+  this.time += this.direction * dt * speed;
 
-    this.vec3.copy(this.entity.getPosition());
-    this.vec3.x = this.defaultPositionX + this.positionXCurve.value(this.time) * 3 * directionX;
+  if (this.time > 1 || this.time < 0) {
+    this.direction *= -1;
+    this.time = pc.math.clamp(this.time, 0, 1);
+  }
 
-    this.entity.rigidbody.teleport(this.vec3);
+  this.vec3.copy(this.entity.getPosition());
+  this.vec3.x =
+    this.defaultPositionX +
+    this.positionXCurve.value(this.time) * 3 * directionX;
+
+  this.entity.rigidbody.teleport(this.vec3);
 };
---
+++ jungle-escape-server/components/teleport-pairs.js
@@ -0,0 +1,32 @@
+var TeleportPair = pc.createScript("teleportPair");
+TeleportPair.attributes.add("target", {
+  type: "entity",
+  title: "Target Entity",
+  description: "The target entity where we are going to teleport",
+});
+
+// initialize code called once per entity
+TeleportPair.prototype.initialize = function () {
+  if (this.target) {
+    this.entity.collision.on("triggerenter", this.onTriggerEnter, this);
+  }
+
+  this.saveInterPols;
+  this.tempInterPols;
+};
+
+// update code called every frame
+TeleportPair.prototype.update = function (dt) {};
+
+// teleport object should be a little gap with the floor.
+TeleportPair.prototype.onTriggerEnter = function (otherEntity) {
+  if (!otherEntity.tags.has("player")) return;
+
+  this.teleport(otherEntity, this.target);
+};
+
+TeleportPair.prototype.teleport = function (playerEntity, targetEntity) {
+  let dstPos = targetEntity.position; //Vec3 ..
+  dstPos.z = dstPos.z - 4;
+  playerEntity.rigidbody.teleport(dstPos);
+};
