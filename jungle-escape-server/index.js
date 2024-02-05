225de2bc87cd49184f7d86fc6b5933eb1c6d6b36
electronyoon <electronyoon@gmail.com>
2024-02-05 16:14:17 +0900

    https reverted

--- jungle-escape-server/index.js
+++ jungle-escape-server/index.js
@@ -3,9 +3,8 @@ import 'dotenv/config';
 import cors from 'cors';
 import path from 'path';
 import express from 'express';
-// import * as https from 'https';
-import * as http from 'http';
-
+import * as https from 'https';
+import fs from 'fs';
 import pn from './custom_modules/playnetwork/src/server/index.js';
 
 import FileLevelProvider from './file-level-provider.js';
@@ -16,11 +15,11 @@ app.get('/pn.js', (_, res) => {
     res.sendFile(path.resolve('./custom_modules/playnetwork/dist/pn.js'));
 });
 
-// const key = fs.readFileSync('./ssl/localhost.key', 'utf8');
-// const cert = fs.readFileSync('./ssl/localhost.crt', 'utf8');
-// const credentials = { key, cert };
+const key = fs.readFileSync('./ssl/localhost.key', 'utf8');
+const cert = fs.readFileSync('./ssl/localhost.crt', 'utf8');
+const credentials = { key, cert };
 
-const server = http.createServer(app);
+const server = https.createServer(credentials, app);
 server.listen(8080, '0.0.0.0');
 
 await pn.start({
