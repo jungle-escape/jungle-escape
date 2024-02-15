586b84ee08eaae724e132456ee47d27cc4b2b7ec
sumin <als980815@gmail.com>
2024-02-16 01:48:53 +0900

    phase3 update

--- jungle-escape-server/components/player-controller.js
+++ jungle-escape-server/components/player-controller.js
@@ -354,6 +354,8 @@ PlayerController.prototype.checkCollisionStartRules = function (hit) {
 
   if (hit.other.tags.has("hammer")) {
     this.entity.collisionTags.push("hammer");
+    var movement = new pc.Vec3(1, 0, 0).scale(300000);
+    this.entity.rigidbody.applyImpulse(movement);
   }
 };
 
