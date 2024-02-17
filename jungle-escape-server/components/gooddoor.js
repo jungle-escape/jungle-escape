dd0a6ef40db5bed134db60c8c2cd83bef44936a3
이병재 <ibyeongjae@ibyeongjaeui-MacBookPro.local>
2024-02-17 15:29:22 +0900

    fix phase2

--- jungle-escape-server/components/gooddoor.js
+++ jungle-escape-server/components/gooddoor.js
@@ -4,12 +4,20 @@ var Gooddoor = pc.createScript("gooddoor");
 Gooddoor.prototype.initialize = function () {
   this.entity.rigidbody.type = pc.BODYTYPE_STATIC;
   this.entity.collision.on("collisionstart", this.onCollisionStart, this);
+  this.triggered = false;
 };
 
 Gooddoor.prototype.onCollisionStart = function (hit) {
+  if (this.triggered) {
+    return;
+  }
+
   if (hit.other.tags.has("player")) {
     this.entity.rigidbody.type = pc.BODYTYPE_DYNAMIC;
+    var impulse = new pc.Vec3(0, 0, -5000);
+    this.entity.rigidbody.applyImpulse(impulse);
     hit.other.collisionTags.push("p1_right");
+    this.triggered = true;
   }
 };
 
