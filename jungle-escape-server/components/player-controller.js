f3116fea6d6345ecd209ff2acd6fc94c0d03a605
electronyoon <electronyoon@gmail.com>
2024-02-18 04:14:31 +0900

    player collision tag added: simple hit

--- jungle-escape-server/components/player-controller.js
+++ jungle-escape-server/components/player-controller.js
@@ -354,11 +354,15 @@ PlayerController.prototype.checkCollisionStartRules = function (hit) {
   if (hit.other.tags.has("rightanswer")) {
     this.entity.collisionTags.push("rightanswer");
   }
-
+  
   if (hit.other.tags.has("hammer")) {
     this.entity.collisionTags.push("hammer");
     var movement = new pc.Vec3(1, 0, 0).scale(300000);
     this.entity.rigidbody.applyImpulse(movement);
+  }
+
+  if (hit.other.tags.has("hit")) {
+    this.entity.collisionTags.push("hit");
   }
 };
 
