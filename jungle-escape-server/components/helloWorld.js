daaef05ca8e1e91160aae2d56c146a312af0f586
sumin <91295349+WantKakao@users.noreply.github.com>
2024-02-17 19:45:26 +0900

    Update helloWorld.js

--- jungle-escape-server/components/helloWorld.js
+++ jungle-escape-server/components/helloWorld.js
@@ -69,16 +69,17 @@ HelloWorld.prototype.update = function (dt) {
           child1.children.forEach((child2) => {
             child2.children.forEach((child3) => {
               if (child3.tags.has("falling_platform")) {
-                child3.rigidbody.type = pc.BODYTYPE_DYNAMIC;
+                child3.destroy();
+                // child3.rigidbody.type = pc.BODYTYPE_DYNAMIC;
 
-                var force = new pc.Vec3(-50, 0, -25); // Y축 방향으로 10의 힘
-                var position = child3.getPosition(); // 힘을 적용할 위치
+                // var force = new pc.Vec3(-50, 0, -25); // Y축 방향으로 10의 힘
+                // var position = child3.getPosition(); // 힘을 적용할 위치
 
-                child3.rigidbody.applyForce(force, position);
-                setTimeout(() => {
-                  // 화살표 함수를 사용하여 this의 컨텍스트를 유지합니다.
-                  child3.destroy();
-                }, 10000); // 10초 후에 실행
+                // child3.rigidbody.applyForce(force, position);
+                // setTimeout(() => {
+                //   // 화살표 함수를 사용하여 this의 컨텍스트를 유지합니다.
+                //   child3.destroy();
+                // }, 10000); // 10초 후에 실행
               }
             });
           });
