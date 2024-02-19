918d69e3c8879da0927e32299cd5bb37406482c7
flowne1 <145897206+flowne1@users.noreply.github.com>
2024-02-19 21:28:53 +0900

    pc_fix_y (#74)
    Co-authored-by: 이병재 <ibyeongjae@ibyeongjaeui-MacBookPro.local>

--- jungle-escape-server/components/obstacles/phase 4/RollingStones.js
+++ jungle-escape-server/components/obstacles/phase 4/RollingStones.js
@@ -1,43 +1,44 @@
-var RollingStones = pc.createScript('rollingStones');
+var RollingStones = pc.createScript("rollingStones");
 
-RollingStones.attributes.add('interval', {
-    type: 'number',
-    enum: [
-        { 'slow': 5 },
-        { 'standard': 3 },
-        { 'fast': 2 },
-        { 'super fast': 1 }
-    ],
-    default: 3
+RollingStones.attributes.add("interval", {
+  type: "number",
+  enum: [{ slow: 5 }, { standard: 3 }, { fast: 2 }, { "super fast": 1 }],
+  default: 3,
 });
-RollingStones.attributes.add('stones', {
-    title: 'Stone templates',
-    type: 'json',
-    array: true,
-    description: 'Templates supposed to work as Network Entity',
-    schema: [{
-        type: 'asset',
-        assetType: 'template',
-        name: 'template'
-    }]
+RollingStones.attributes.add("stones", {
+  title: "Stone templates",
+  type: "json",
+  array: true,
+  description: "Templates supposed to work as Network Entity",
+  schema: [
+    {
+      type: "asset",
+      assetType: "template",
+      name: "template",
+    },
+  ],
 });
 
 RollingStones.prototype.initialize = function () {
-    this.time = 0;
-    this.app.on('_player:arrived', () => {
-        this.interval = setInterval(() => {
-            this.onDynamic();
-        }, this.interval * 1000);
-    }, this);
-    this.once('destroy', () => {
-        clearInterval(this.interval);
-    });
-    this._stones = this.stones.map(t => t.template.resource);
-}
+  this.time = 0;
+  this.app.on(
+    "_player:arrived",
+    () => {
+      this.intervalId = setInterval(() => {
+        this.onDynamic();
+      }, this.interval * 1000);
+    },
+    this
+  );
+  this.once("destroy", () => {
+    clearInterval(this.intervalId);
+  });
+  this._stones = this.stones.map((t) => t.template.resource);
+};
 
 RollingStones.prototype.onDynamic = function () {
-    const stone = this._stones[Math.floor(Math.random() * this._stones.length)];
-    const entity = stone.instantiate(this.app);
-    entity.enabled = true;
-    this.entity.addChild(entity);
-}
+  const stone = this._stones[Math.floor(Math.random() * this._stones.length)];
+  const entity = stone.instantiate(this.app);
+  entity.enabled = true;
+  this.entity.addChild(entity);
+};
