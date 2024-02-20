b6f57eef13804ea5eceb2b58164ef1256c8d77c2
flowne1 <145897206+flowne1@users.noreply.github.com>
2024-02-20 12:13:51 +0900

    Update RollingStones.js

--- jungle-escape-server/components/obstacles/phase 4/RollingStones.js
+++ jungle-escape-server/components/obstacles/phase 4/RollingStones.js
@@ -21,24 +21,40 @@ RollingStones.attributes.add("stones", {
 
 RollingStones.prototype.initialize = function () {
   this.time = 0;
-  this.app.on(
-    "_player:arrived",
-    () => {
-      this.intervalId = setInterval(() => {
-        this.onDynamic();
-      }, this.interval * 1000);
-    },
-    this
-  );
+  this.stoneEntities = [];
+  this.playerArrivedHandler = () => {
+    // Store the function reference
+    this.intervalId = setInterval(() => {
+      this.onDynamic();
+    }, this.interval * 1000);
+  };
+  this.app.on("_player:arrived", this.playerArrivedHandler, this);
+
   this.once("destroy", () => {
     clearInterval(this.intervalId);
+    this.app.off("_player:arrived", this.playerArrivedHandler, this); // Use .off with the stored reference
   });
+
   this._stones = this.stones.map((t) => t.template.resource);
 };
 
 RollingStones.prototype.onDynamic = function () {
   const stone = this._stones[Math.floor(Math.random() * this._stones.length)];
   const entity = stone.instantiate(this.app);
+  this.stoneEntities.push(entity);
   entity.enabled = true;
   this.entity.addChild(entity);
+
+  setTimeout(() => {
+    // 엔티티 파괴 전에 배열에서 제거
+    const index = this.stoneEntities.indexOf(entity);
+    if (index > -1) {
+      this.stoneEntities.splice(index, 1);
+    }
+
+    // 엔티티가 이미 파괴되지 않았는지 확인
+    if (entity && !entity.destroyed) {
+      entity.destroy();
+    }
+  }, 15000);
 };
