59e33ea8ca74cbe18f3d836c7e0ec8824324fa56
electronyoon <electronyoon@gmail.com>
2024-02-22 10:40:46 +0900

    refactor structure

--- jungle-escape-server/components/obstacles/DecayObject.js
+++ jungle-escape-server/components/obstacles/DecayObject.js
@@ -17,29 +17,38 @@ DecayObject.prototype.initialize = function () {
     this.entity.render.material = this.entity.render.material.clone();
     this.entity.render.material.update();
 
-    const generate = setTimeout(() => {
-        this.onCollisionStart();
-    }, 2000);
-    this.entity.once('destroy', () => {
-        clearTimeout(generate);
-    });
-    this.entity.rigidbody.on('collisionstart', generate, this);
+    this.tweenTimeoutEvent = (second) => {
+        return setTimeout(() => {
+            // 1. delete hit tags as decay start
+            this.entity.tags.clear();
+
+            // 2. decay effect
+            this.fromDiffuse = new pc.Color(1, 1, 1);
+            this.toDiffuse = new pc.Color(0.3, 0.3, 0.3);
+        
+            // 3. decay animation
+            this.entity.tween(this.fromDiffuse)
+                .to(this.toDiffuse, 1, pc.ExponentialOut)
+                .onUpdate(() => {
+                    this.entity.render.material.diffuse = this.fromDiffuse;
+                    this.entity.render.material.update();
+                })
+                .onComplete(() => {
+                    this.entity.rigidbody.group = 2;
+                })
+                .start();
+        }, second * 1000);
+    };
+
+    this.entity.rigidbody.on('collisionstart', this.onCollisionStart, this);
 }
 
 DecayObject.prototype.onCollisionStart = function () {
     this.entity.rigidbody.off('collisionstart');
-    
-    this.fromDiffuse = new pc.Color(1, 1, 1);
-    this.toDiffuse = new pc.Color(0, 0, 0);
-    this.entity.tween(this.fromDiffuse)
-        .to(this.toDiffuse, 5, pc.ExponentialOut)
-        .onUpdate(() => {
-            this.entity.render.material.diffuse = this.fromDiffuse;
-            this.entity.render.material.update();
-        })
-        .onComplete(() => {
-            this.entity.tags.clear();
-            this.entity.rigidbody.group = 2;
-        })
-        .start();
+
+    const decayTimer = this.tweenTimeoutEvent(6);
+    this.entity.once('destroy', () => {
+        clearTimeout(decayTimer);
+    });
+    decayTimer();
 }
